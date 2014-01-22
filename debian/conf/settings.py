fb5b1822e0fe09bdb9915c9940ec1d68d99ab028
geoffrey jost <gjost@ddrpub>
2014-01-21 17:27:21 -0800

    adapted settings from ddr-local

--- debian/conf/settings.py
+++ debian/conf/settings.py
@@ -1,117 +1,83 @@
-# Django settings for ddrpublic project.
+# Django settings for ddrpublic.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+# ----------------------------------------------------------------------
+
+import ConfigParser
+import logging
+import os
+
+class NoConfigError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+CONFIG_FILE = '/etc/ddr/ddr.cfg'
+if not os.path.exists(CONFIG_FILE):
+    raise NoConfigError('No config file!')
+config = ConfigParser.ConfigParser()
+config.read(CONFIG_FILE)
+
+LOG_FILE='/var/log/ddr/public.log'
+LOG_LEVEL=DEBUG
+
+SECRET_KEY='ivb=&yqv^#%_i5v&c*qa@aaa#=z-(t$i=k*geugb_!2ky&j0ca'
+
+LANGUAGE_CODE='en-us'
+TIME_ZONE='America/Los_Angeles'
+
+# Filesystem path and URL for static media (mostly used for interfaces).
+STATIC_ROOT='/var/www/static'
+STATIC_URL='/static/'
+
+# Filesystem path and URL for media to be manipulated by ddrlocal
+# (collection repositories, thumbnail cache, etc).
+MEDIA_ROOT='/var/www/media'
+MEDIA_URL='/media/'
+
+ACCESS_FILE_APPEND='-a'
+ACCESS_FILE_EXTENSION='.jpg'
+ACCESS_FILE_GEOMETRY='1024x1024>'
+ACCESS_FILE_OPTIONS=''
+THUMBNAIL_GEOMETRY='512x512>'
+THUMBNAIL_OPTIONS=''
+
+# Directory in root of USB HDD that marks it as a DDR disk
+# /media/USBHDDNAME/ddr
+DDR_USBHDD_BASE_DIR = 'ddr'
+
+MEDIA_BASE = os.path.join(MEDIA_ROOT, 'base')
+
+
+ENTITY_FILE_ROLES = (
+    ('master','master'),
+    ('mezzanine','mezzanine'),
+    ('access','access'),
 )
 
+DATE_FORMAT = '%Y-%m-%d'
+TIME_FORMAT = '%H:%M:%S'
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S:%f'
+# Django uses a slightly different datetime format
+DATETIME_FORMAT_FORM = '%Y-%m-%d %H:%M:%S'
+
+PRETTY_DATE_FORMAT = '%d %B %Y'
+PRETTY_TIME_FORMAT = '%I:%M %p'
+PRETTY_DATETIME_FORMAT = '%d %B %Y, %I:%M %p'
+
+# ----------------------------------------------------------------------
+
+ADMINS = (
+    ('geoffrey jost', 'geoffrey.jost@densho.org'),
+    ('Geoff Froh', 'geoff.froh@densho.org'),
+)
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
-USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'pxf=q$-yd*&e*c9jkjp*dduhg&!(-l2037+o7&&)$dovw09m3s'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'ddrpublic.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'ddrpublic.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -120,37 +86,164 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    #'django.contrib.admin',
+    #
+    'sorl.thumbnail',
+    #
+    'ddrpublic',
+    'elasticsearch',
+    'ui',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/lib/ddr/ddrpublic.db',
+    }
+}
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+REDIS_DB_CACHE = 0
+REDIS_DB_CELERY_BROKER = 1
+REDIS_DB_CELERY_RESULT = 2
+REDIS_DB_SORL = 3
+
+CACHES = {
+    "default": {
+        "BACKEND": "redis_cache.cache.RedisCache",
+        "LOCATION": "%s:%s:%s" % (REDIS_HOST, REDIS_PORT, REDIS_DB_CACHE),
+        "OPTIONS": {
+            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
+        }
+    }
+}
+
+# ElasticSearch
+ELASTICSEARCH_HOST = 'localhost'
+ELASTICSEARCH_PORT = '9200'
+
+# sorl-thumbnail
+THUMBNAIL_DEBUG = DEBUG
+THUMBNAIL_KVSTORE = 'sorl.thumbnail.kvstores.redis_kvstore.KVStore'
+#THUMBNAIL_KVSTORE = 'sorl.thumbnail.kvstores.cached_db_kvstore.KVStore'
+THUMBNAIL_REDIS_PASSWORD = ''
+THUMBNAIL_REDIS_HOST = REDIS_HOST
+THUMBNAIL_REDIS_PORT = int(REDIS_PORT)
+THUMBNAIL_REDIS_DB = REDIS_DB_SORL
+THUMBNAIL_ENGINE = 'sorl.thumbnail.engines.convert_engine.Engine'
+THUMBNAIL_CONVERT = 'convert'
+THUMBNAIL_IDENTIFY = 'identify'
+THUMBNAIL_CACHE_TIMEOUT = 60*60*24*365*10  # 10 years
+
+SESSION_ENGINE = 'redis_sessions.session'
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+TEMPLATE_DIRS = (
+    '/usr/local/src/ddr-local/ddrpublic/ui/templates',
+)
+
+STATICFILES_DIRS = (
+    #'/opt/ddr-local/ddrpublic/ddrpublic/static',
+    #'/usr/local/src/ddr-local/ddrpublic/storage/static',
+    '/usr/local/src/ddr-local/ddrpublic/ui/static',
+)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)-8s [%(module)s.%(funcName)s]  %(message)s'
+        },
+        'simple': {
+            'format': '%(asctime)s %(levelname)-8s %(message)s'
+        },
+    },
     'filters': {
+        # only log when settings.DEBUG == False
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
-        }
+        },
     },
     'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'file': {
+            'level': LOG_LEVEL,
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': LOG_FILE,
+            'when': 'D',
+            'backupCount': 14,
+            'formatter': 'verbose',
+        },
         'mail_admins': {
             'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
             'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+            'formatter': 'verbose',
+        },
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+            'handlers': ['mail_admins'],
         },
-    }
+    },
+    # This is the only way I found to write log entries from the whole DDR stack.
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['file'],
+    },
 }
+
+USE_TZ = True
+USE_I18N = True
+USE_L10N = True
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    #'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'ddrpublic.urls'
+
+WSGI_APPLICATION = 'ddrpublic.wsgi.application'
