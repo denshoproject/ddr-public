3bfe3225c7ffcf12eb429398d5795001165d75a0
geoffrey jost <geoffrey.jost@densho.org>
2014-01-24 05:58:55 -0800

    ui views,templates use data from elasticsearch queries,GET

--- ddrpublic/ui/views/collections.py
+++ ddrpublic/ui/views/collections.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import Http404, get_object_or_404, render_to_response
 from django.template import RequestContext
 
+import elasticsearch
+
 
 # views ----------------------------------------------------------------
 
@@ -22,12 +24,30 @@ def list( request, repo, org ):
     )
 
 def detail( request, repo, org, cid ):
+    collection_id='%s-%s-%s' % (repo, org, cid)
+    get = elasticsearch.get(
+        index='ddr',
+        model='collection',
+        id=collection_id,
+    )
+    obj = []
+    for field in get:
+        for k,v in field.iteritems():
+            obj.append((k,v))
+    entities = elasticsearch.massage_hits(
+        elasticsearch.query(
+            model='entity',
+            query='id:"%s-%s-%s"' % (repo, org, cid),
+        )
+    )
     return render_to_response(
         'ui/collections/detail.html',
         {
             'repo': repo,
             'org': org,
             'cid': cid,
+            'collection': obj,
+            'entities': entities,
         },
         context_instance=RequestContext(request, processors=[])
     )
--- ddrpublic/ui/views/files.py
+++ ddrpublic/ui/views/files.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import Http404, get_object_or_404, render_to_response
 from django.template import RequestContext
 
+import elasticsearch
+
 
 # views ----------------------------------------------------------------
 
@@ -24,6 +26,16 @@ def list( request, repo, org, cid, eid ):
     )
 
 def detail( request, repo, org, cid, eid, role, sha1 ):
+    file_id='%s-%s-%s-%s-%s-%s' % (repo, org, cid, eid, role, sha1)
+    get = elasticsearch.get(
+        index='ddr',
+        model='file',
+        id=file_id,
+    )
+    obj = []
+    for field in get:
+        for k,v in field.iteritems():
+            obj.append((k,v))
     return render_to_response(
         'ui/files/detail.html',
         {
@@ -33,6 +45,7 @@ def detail( request, repo, org, cid, eid, role, sha1 ):
             'eid': eid,
             'role': role,
             'sha1': sha1,
+            'file': obj,
         },
         context_instance=RequestContext(request, processors=[])
     )
