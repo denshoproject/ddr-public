f6897838942fa2d7b19d2ebb2328b9e87464eba9
geoffrey jost <geoffrey.jost@densho.org>
2014-01-25 23:50:36 -0800

    facet terms list

--- ddrpublic/ui/views/browse.py
+++ ddrpublic/ui/views/browse.py
@@ -8,6 +8,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import Http404, get_object_or_404, render_to_response
 from django.template import RequestContext
 
+from DDR import elasticsearch
+
 
 # views ----------------------------------------------------------------
 
@@ -19,10 +21,12 @@ def index( request ):
     )
 
 def facet( request, facet ):
+    results = elasticsearch.facet_terms(settings.ELASTICSEARCH_HOST_PORT, 'ddr', facet, order='term')
     return render_to_response(
         'ui/browse/facet.html',
         {
             'facet': facet,
+            'results': results,
         },
         context_instance=RequestContext(request, processors=[])
     )
