e3463f693b91c9ea94cdb86e4872663cdf2a3168
geoffrey jost <geoffrey.jost@densho.org>
2014-02-20 15:06:49 -0800

    repo and org detail pages redirect to /collections/

--- ddrpublic/ui/views/organizations.py
+++ ddrpublic/ui/views/organizations.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import Http404, get_object_or_404, render_to_response
+from django.shortcuts import Http404, get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 
 from ui.models import Repository, Organization, Collection, Entity, File
@@ -23,15 +23,16 @@ def list( request, repo ):
     )
 
 def detail( request, repo, org ):
-    organization = Organization.get(repo, org)
-    collections = organization.collections()
-    return render_to_response(
-        'ui/organizations/detail.html',
-        {
-            'repo': repo,
-            'org': org,
-            'organization': organization,
-            'collections': collections,
-        },
-        context_instance=RequestContext(request, processors=[])
-    )
+    return redirect('ui-collections-list')
+    #organization = Organization.get(repo, org)
+    #collections = organization.collections()
+    #return render_to_response(
+    #    'ui/organizations/detail.html',
+    #    {
+    #        'repo': repo,
+    #        'org': org,
+    #        'organization': organization,
+    #        'collections': collections,
+    #    },
+    #    context_instance=RequestContext(request, processors=[])
+    #)
--- ddrpublic/ui/views/repo.py
+++ ddrpublic/ui/views/repo.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import Http404, get_object_or_404, render_to_response
+from django.shortcuts import Http404, get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 
 from ui.models import Repository, Organization, Collection, Entity, File
@@ -14,14 +14,15 @@ from ui.models import Repository, Organization, Collection, Entity, File
 # views ----------------------------------------------------------------
 
 def detail( request, repo ):
-    repository = Repository.get(repo)
-    organizations = repository.organizations()
-    return render_to_response(
-        'ui/repo/detail.html',
-        {
-            'repo': repo,
-            'repository': repository,
-            'organizations': organizations,
-        },
-        context_instance=RequestContext(request, processors=[])
-    )
+    return redirect('ui-collections-list')
+    #repository = Repository.get(repo)
+    #organizations = repository.organizations()
+    #return render_to_response(
+    #    'ui/repo/detail.html',
+    #    {
+    #        'repo': repo,
+    #        'repository': repository,
+    #        'organizations': organizations,
+    #    },
+    #    context_instance=RequestContext(request, processors=[])
+    #)
