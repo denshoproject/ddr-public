e4bbae02ed56add38a83a74ca8c7bb0642a65718
geoffrey jost <geoffrey.jost@densho.org>
2014-02-27 10:44:11 -0800

    use common template for listing objects throughout site
    - Templatetags for collection,entity,file.
    - Bootstrap media objects

--- ddrpublic/ui/templatetags/ui_tags.py
+++ ddrpublic/ui/templatetags/ui_tags.py
@@ -4,12 +4,30 @@ from django import template
 register = template.Library()
 
 
+def collection( obj ):
+    """list-view collection template
+    """
+    t = template.loader.get_template('ui/collections/list-object.html')
+    return t.render(template.Context({'object':obj}))
+
+def entity( obj ):
+    """list-view entity template
+    """
+    t = template.loader.get_template('ui/entities/list-object.html')
+    return t.render(template.Context({'object':obj}))
+
+def file( obj ):
+    """list-view file template
+    """
+    t = template.loader.get_template('ui/files/list-object.html')
+    return t.render(template.Context({'object':obj}))
+
+
 def addthis():
     """AddThis button
     """
     t = template.loader.get_template('ui/addthis.html')
     return t.render(template.Context({}))
-
 
 def cite( url ):
     """Citation tag
@@ -18,5 +36,8 @@ def cite( url ):
     c = template.Context({'url':url})
     return t.render(c)
 
+register.simple_tag(collection)
+register.simple_tag(entity)
+register.simple_tag(file)
 register.simple_tag(addthis)
 register.simple_tag(cite)
