a7776b9d65f93c4d00c29ff5b730f2b28b470efb
geoffrey jost <geoffrey.jost@densho.org>
2014-04-02 12:58:02 -0700

    print time and info about app server (pid, host, commit) in base.html

--- ddrpublic/ui/context_processors.py
+++ ddrpublic/ui/context_processors.py
@@ -1,10 +1,23 @@
+from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
+import os
+
+import envoy
 
 from django.conf import settings
 
 from ui.forms import SearchForm
 
+
+def git_commit():
+    """Returns the ddr-local repo's most recent Git commit.
+    """
+    try:
+        commit = envoy.run('git log --pretty=format:"%H" -1').std_out
+    except:
+        commit = 'unknown'
+    return commit
 
 def sitewide(request):
     """Variables that need to be inserted into all templates.
@@ -13,4 +26,8 @@ def sitewide(request):
         'request': request,
         'hide_header_search': False,
         'search_form': SearchForm,
+        'time': datetime.now().isoformat(),
+        'pid': os.getpid(),
+        'host': os.uname()[1],
+        'commit': git_commit()[:7],
     }
