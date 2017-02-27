642a0574a3c017f5ffa0717729ac13e080138723
geoffrey jost <geoffrey@jostwebwerks.com>
2017-02-27 13:28:45 -0800

    views.entity: allow for old/original entity.format value

--- ddrpublic/ui/views/entities.py
+++ ddrpublic/ui/views/entities.py
@@ -26,6 +26,11 @@ def detail(request, oid):
         raise Http404
     
     # if this is an interview, redirect to first segment
+    format_ = None
+    if entity.get('format') and isinstance(entity['format'], basestring):
+        format_ = entity['format']
+    elif entity.get('format') and isinstance(entity['format'], dict):
+        format_ = entity['format']['id']  # format is wrapped in ApiEntity.api_get
     format_ = entity['format']['id']  # format is wrapped in ApiEntity.api_get
     if format_ == 'vh':
         if i.model == 'segment':
