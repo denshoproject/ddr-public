ff0a18613f9212fa8e8b6ddedbfadee47ea1e179
geoffrey jost <geoffrey@jostwebwerks.com>
2018-04-13 10:40:28 -0700

    views: get organization, narrator w/o identifier

--- ddrpublic/ui/views/organizations.py
+++ ddrpublic/ui/views/organizations.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import Http404, get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
 
-from ui.models import Organization
+from ui import api
 
 
 # views ----------------------------------------------------------------
@@ -22,8 +22,11 @@ def list(request, oid):
     )
 
 def detail(request, oid):
-    organization = Organization.get(oid)
-    collections = organization.children()
+    organization = api.Organization.get(organization_id)
+    collections = api.Organization.children(
+        org['id'], request,
+        limit=settings.ELASTICSEARCH_MAX_SIZE,
+    )
     return render_to_response(
         'ui/organizations/detail.html',
         {
