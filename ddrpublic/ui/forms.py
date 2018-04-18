936aaa66f656d259155e0bf01cba573fe900a292
geoffrey jost <geoffrey@jostwebwerks.com>
2018-04-18 16:21:13 -0700

    forms,models: move search form choices to models

--- ddrpublic/ui/forms.py
+++ ddrpublic/ui/forms.py
@@ -8,141 +8,6 @@ from django.core.cache import cache
 from ui import docstore
 from ui import models
 
-# TODO REPLACE THESE HARDCODED VALUES!!!
-
-MODELS_CHOICES = [
-    ('collection', 'Collection'),
-    ('entity', 'Entity'),
-    ('segment', 'Segment'),
-    ('file', 'File'),
-    ('narrator', 'Narrator'),
-    ('term', 'Topic Term'),
-]
-
-LANGUAGE_CHOICES = [
-    ('eng', 'English'),
-    ('jpn', 'Japanese'),
-    ('chi', 'Chinese'),
-]
-
-FORMAT_CHOICES = [
-    ['av','Audio/Visual'],
-    ['ds','Dataset'],
-    ['doc','Document'],
-    ['img','Still Image'],
-    ['vh','Oral History'],
-]
-
-RIGHTS_CHOICES = [
-    ["cc", "DDR Creative Commons"],
-    ["pcc", "Copyright, with special 3rd-party grant permitted"],
-    ["nocc", "Copyright restricted"],
-    ["pdm", "Public domain" ],
-]
-
-GENRE_CHOICES = [
-    ['advertisement','Advertisements'],
-    ['album','Albums'],
-    ['architecture','Architecture'],
-    ['baseball_card','Baseball Cards'],
-    ['blank_form','Blank Forms'],
-    ['book','Books'],
-    ['broadside','Broadsides'],
-    ['cartoon','Cartoons (Commentary)'],
-    ['catalog','Catalogs'],
-    ['cityscape','Cityscapes'],
-    ['clipping','Clippings'],
-    ['correspondence','Correspondence'],
-    ['diary','Diaries'],
-    ['drawing','Drawings'],
-    ['ephemera','Ephemera'],
-    ['essay','Essays'],
-    ['ethnography','Ethnography'],
-    ['fieldnotes','Fieldnotes'],
-    ['illustration','Illustrations'],
-    ['interview','Interviews'],
-    ['landscape','Landscapes'],
-    ['leaflet','Leaflets'],
-    ['manuscript','Manuscripts'],
-    ['map','Maps'],
-    ['misc_document','Miscellaneous Documents'],
-    ['motion_picture','Motion Pictures'],
-    ['music','Music'],
-    ['narrative','Narratives'],
-    ['painting','Paintings'],
-    ['pamphlet','Pamphlets'],
-    ['periodical','Periodicals'],
-    ['petition','Petitions'],
-    ['photograph','Photographs'],
-    ['physical_object','Physical Objects'],
-    ['poetry','Poetry'],
-    ['portrait','Portraits'],
-    ['postcard','Postcards'],
-    ['poster','Posters'],
-    ['print','Prints'],
-    ['program','Programs'],
-    ['rec_log','Recording Logs'],
-    ['score','Scores'],
-    ['sheet_music','Sheet Music'],
-    ['timetable','Timetables'],
-    ['transcription','Transcriptions'],
-]
-
-
-def flatten(path, indent='--'):
-    paths = [x for x in path.split(':')]
-    # TODO replace instead of make new list
-    flat = []
-    for x in paths[:-1]:
-        flat.append(indent)
-    flat.append(paths[-1])
-    return ''.join(flat)
-
-def topics_flattened():
-    oid = 'topics'
-    request = None
-    key = 'search:filters:%s' % oid
-    cached = cache.get(key)
-    if not cached:
-        terms = models.Facet.make_tree(
-            models.Facet.children(
-                oid, request,
-                sort=[('sort','asc')],
-                limit=10000, raw=True
-            )['objects']
-        )
-        choices = [
-            (term['id'], term['path'])
-            for term in terms
-        ]
-        cached = choices
-        cache.set(key, cached, 15) # settings.ELASTICSEARCH_QUERY_TIMEOUT)
-    return cached
-
-TOPICS_CHOICES = topics_flattened()
-
-def facilities():
-    oid = 'facility'
-    request = None
-    key = 'search:filters:%s' % oid
-    cached = cache.get(key)
-    if not cached:
-        terms = models.Facet.children(
-            oid, request,
-            sort=[('title','asc')],
-            limit=10000, raw=True
-        )['objects']
-        # for some reason ES does not sort
-        terms = sorted(terms, key=lambda term: term['title'])
-        cached = [
-            (term['id'], term['title'])
-            for term in terms
-        ]
-        cache.set(key, cached, 15) # settings.ELASTICSEARCH_QUERY_TIMEOUT)
-    return cached
-
-FACILITY_CHOICES = facilities()
-
 class SearchForm(forms.Form):
     
     fulltext = forms.CharField(
@@ -158,23 +23,23 @@ class SearchForm(forms.Form):
     )
     
     filter_format = forms.MultipleChoiceField(
-        choices=FORMAT_CHOICES,
+        choices=models.FORMAT_CHOICES,
         required=False,
     )
     filter_genre = forms.MultipleChoiceField(
-        choices=GENRE_CHOICES,
+        choices=models.GENRE_CHOICES,
         required=False,
     )
     filter_topics = forms.MultipleChoiceField(
-        choices=TOPICS_CHOICES,
+        choices=models.topics_flattened(),
         required=False,
     )
     filter_facility = forms.MultipleChoiceField(
-        choices=FACILITY_CHOICES,
+        choices=models.facilities(),
         required=False,
     )
     filter_rights = forms.MultipleChoiceField(
-        choices=RIGHTS_CHOICES,
+        choices=models.RIGHTS_CHOICES,
         required=False,
     )
     
