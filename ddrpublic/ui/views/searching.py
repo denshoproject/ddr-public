b486fc7874160a24296a554c8c6b8ed93b0a5d73
geoffrey jost <geoffrey@jostwebwerks.com>
2018-05-10 18:13:17 -0700

    views.searching: adapt from ddr-local

--- ddrpublic/ui/views/searching.py
+++ ddrpublic/ui/views/searching.py
@@ -11,9 +11,9 @@ from django.template import RequestContext
 from elasticsearch.exceptions import ConnectionError, ConnectionTimeout
 
 from ui import api
-from ui import docstore
-from ui import search
 from ui import forms_search as forms
+from ui import models
+from ui import search
 
 
 def _mkurl(request, path, query=None):
@@ -25,15 +25,19 @@ def _mkurl(request, path, query=None):
     
 def search_ui(request):
     api_url = '%s?%s' % (
-        _mkurl(request, reverse('api-search')),
+        _mkurl(request, reverse('ui-api-search')),
         request.META['QUERY_STRING']
     )
     context = {
+        'template_extends': 'ui/search/base.html',
+        'searching': False,
+        'filters': True,
         'api_url': api_url,
     }
-
+    
     if request.GET.get('fulltext'):
-
+        context['searching'] = True
+        
         if request.GET.get('offset'):
             # limit and offset args take precedence over page
             limit = request.GET.get('limit', int(request.GET.get('limit', settings.RESULTS_PER_PAGE)))
@@ -47,28 +51,32 @@ def search_ui(request):
             offset = 0
         
         searcher = search.Searcher(
-            mappings=identifier.ELASTICSEARCH_CLASSES_BY_MODEL,
-            fields=identifier.ELASTICSEARCH_LIST_FIELDS,
+            #mappings=identifier.ELASTICSEARCH_CLASSES_BY_MODEL,
+            #fields=identifier.ELASTICSEARCH_LIST_FIELDS,
         )
         searcher.prepare(request)
         results = searcher.execute(limit, offset)
-        context['results'] = results
-        context['search_form'] = forms.SearchForm(
+        form = forms.SearchForm(
+            data=request.GET.copy(),
             search_results=results,
-            data=request.GET
         )
+        context['results'] = results
+        context['search_form'] = form
         
         if results.objects:
             paginator = Paginator(
                 results.ordered_dict(
-                    request=request, list_function=api._prep_detail, pad=True
+                    request=request,
+                    list_function=models.format_object_detail2,
+                    pad=True,
                 )['objects'],
                 results.page_size,
             )
+            page = paginator.page(results.this_page)
             context['paginator'] = paginator
-            context['page'] = paginator.page(results.this_page)
+            context['page'] = page
 
     else:
         context['search_form'] = forms.SearchForm()
-    
-    return render(request, 'webui/search/search.html', context)
+
+    return render(request, 'ui/search/results.html', context)
