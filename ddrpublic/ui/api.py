aa20b8af3be9d813ba48c7066ecb17b6228f62cc
Geoffrey Jost <geoffrey@jostwebwerks.com>
2019-04-23 13:16:17 -0700

    api: Fix typo in facetterm view

--- ddrpublic/ui/api.py
+++ ddrpublic/ui/api.py
@@ -349,7 +349,7 @@ def facet(request, facet_id, format=None):
 @api_view(['GET'])
 def facetterm(request, facet_id, term_id, format=None):
     object_id = '%s-%s' % (facet_id, term_id)
-    data = Term.get(oid, request)
+    data = Term.get(object_id, request)
     return _detail(request, data)
 
 
