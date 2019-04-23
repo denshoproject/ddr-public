4a281140cc240f4d7472cce8becf045ca19f3183
Geoffrey Jost <geoffrey@jostwebwerks.com>
2019-04-23 13:11:47 -0700

    settings: Set rest-framework DEFAULT_PAGINATION_CLASS

--- conf/settings.py
+++ conf/settings.py
@@ -241,6 +241,7 @@ REST_FRAMEWORK = {
         'anon': THROTTLE_ANON,
         'user': THROTTLE_USER,
     },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 
