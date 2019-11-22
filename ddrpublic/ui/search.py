4a2dd33717ca8700e8afec90e52969f97e519d3e
Geoffrey Jost <geoffrey@jostwebwerks.com>
2019-11-22 11:20:55 -0800

    search: Add fields to default SEARCH_INCLUDE_FIELDS

--- ddrpublic/ui/search.py
+++ ddrpublic/ui/search.py
@@ -100,7 +100,13 @@ NAMESDB_SEARCH_MODELS = ['names-record']
 # fields searched by query e.g. query will find search terms in these fields
 # TODO move to ddr-defs/repo_models/elastic.py?
 SEARCH_INCLUDE_FIELDS = [
+    'id',
     'model',
+    'links_html',
+    'links_json',
+    'links_img',
+    'links_thumb',
+    'links_children',
     'status',
     'public',
     'title',
