0e2ff3ed075e45cd03d66b2c4770968710aa5baa
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-06-04 14:05:05 -0700

    Replace error* views with handler*

--- ddrpublic/ui/urls.py
+++ ddrpublic/ui/urls.py
@@ -107,9 +107,9 @@ urlpatterns = [
     path('', index, name='ui-index'),
 ]
 
-handler400 = 'ui.views.error400'
-handler403 = 'ui.views.error403'
-handler404 = 'ui.views.error404'
-handler500 = 'ui.views.error500'
+handler400 = 'ui.views.handler400'
+handler403 = 'ui.views.handler403'
+handler404 = 'ui.views.handler404'
+handler500 = 'ui.views.handler500'
 
 urlpatterns = format_suffix_patterns(urlpatterns)
