b50fc2c85b07c9bfd8a50b19d399ee71dd4e5410
Geoffrey Jost <geoffrey@jostwebwerks.com>
2021-01-08 12:47:26 -0800

    models: Replace MEDIA_URL with BACKBLAZE_URL for b2 downloads

--- ddrpublic/ui/models.py
+++ ddrpublic/ui/models.py
@@ -438,11 +438,14 @@ def format_object_detail2(document, request, listitem=False):
         d['links']['img'] = '%s%s' % (settings.MEDIA_URL, document.pop('links_img'))
         d['links']['thumb'] = '%s%s' % (settings.MEDIA_URL_LOCAL, document.pop('links_thumb'))
         if document.get('links_download'):
-            #d['links']['download'] = '%s%s' % (settings.MEDIA_URL, links_download)
-            # Backblaze
-            d['links']['download'] = '{}{}'.format(
-                settings.BACKBLAZE_BUCKET_URL, document.pop('links_download')
-            )
+            if document.get('backblaze'):
+                d['links']['download'] = '{}{}'.format(
+                    settings.BACKBLAZE_BUCKET_URL, document.pop('links_download')
+                )
+            else:
+                d['links']['download'] = '{}{}'.format(
+                    settings.MEDIA_URL, document.pop('links_download')
+                )
     
     if not listitem:
         if document.get('parent_id'):
