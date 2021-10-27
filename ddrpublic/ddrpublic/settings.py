c8fda9382b19d8801680de14e5303964c38542bd
Geoffrey Jost <geoffrey@jostwebwerks.com>
2021-10-27 13:28:55 -0700

    settings: Log w WatchedFileHandler; let logrotate handle rotation

--- ddrpublic/ddrpublic/settings.py
+++ ddrpublic/ddrpublic/settings.py
@@ -421,10 +421,8 @@ LOGGING = {
         },
         'file': {
             'level': LOG_LEVEL,
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': '/var/log/ddr/public.log',
-            'when': 'D',
-            'backupCount': 14,
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/ddr/ddrpublic.log',
             'filters': ['request_id'],
             'formatter': 'verbose',
         },
